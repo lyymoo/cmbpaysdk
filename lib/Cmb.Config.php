4d74e5bb7d201d354c641ac6f5cb67284f701a34
mozu <limeng@sipc.net.cn>
2018-09-10 14:23:57 +0800

    cmb

---
+++ lib/Cmb.Config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* 配置网关信息
+* @author moz
+*/
+
+class CmbConfig
+{
+    //=======【基本信息设置】=====================================
+    //
+    /**
+     * MERKEY: 支付密钥（商户签名密钥限定16位）
+     * @var string
+     */
+    const MERKEY = 'XXXXXXXXXXXXXXXX';
+    /**
+     * branchNo: 商户分行号，4位数字
+     * @var string
+     */
+    const BRANCHNO = 'XXXX';
+    /**
+     * merchantNo: 商户号，6位数字
+     * @var string
+     */
+    const MERCHANTNO = 'XXXXXX';
+}
